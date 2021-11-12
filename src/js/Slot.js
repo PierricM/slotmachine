79f1a35cc538d3d568a93d39b2e66f6e09939972
Pierric Maillard <pierric.maillard@dymonasia.com>
2021-11-12 14:51:25 +0800

    first comit

--- src/js/Slot.js
+++ src/js/Slot.js
@@ -6,19 +6,19 @@ export default class Slot {
     Symbol.preload();
 
     this.currentSymbols = [
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
+      ["orca", "orca", "orca"],
+      ["orca", "orca", "orca"],
+      ["orca", "orca", "orca"],
+      ["orca", "orca", "orca"],
+      ["orca", "orca", "orca"],
     ];
 
     this.nextSymbols = [
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
+      ["orca", "orca", "orca"],
+      ["orca", "orca", "orca"],
+      ["orca", "orca", "orca"],
+      ["orca", "orca", "orca"],
+      ["orca", "orca", "orca"],
     ];
 
     this.container = domElement;
